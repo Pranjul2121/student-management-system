314ab81795098d14f095432b96b53d193841c78e
Pranjul Tiwari <pranjult21@gmail.com>
2026-09-24 23:17:52 +0530

    Adding DOM

---
+++ DOM/script.js
@@ -0,0 +1,26 @@
+// by get element by id
+
+// const heading1=document.getElementById("heading");
+// console.log("heading1")
+
+// const heading2=document.getElementByTagName("h1");
+// console.log("heading2")
+
+// const heading3=document.getElementsByClassName("heading");
+// console.log("heading3")
+
+// const heading4=document.querySelector(".headingClass");
+// console.log("heading4")
+
+//innerText vs innerHTML vs textContent
+// #heading particularly used for id selector
+// .headingClass particularly used for class selector
+
+// const headingElement=document.querySelector("#heading");
+// console.log(headingElement.innerText);
+// console.log(headingElement.innerHTML);
+// console.log(headingElement.textContent);
+
+//Homework
+// event listeners
+// event propagation and phases in events->bubbling,capturing,event target
